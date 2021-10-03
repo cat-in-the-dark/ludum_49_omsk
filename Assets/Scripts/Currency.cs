26ce56a9c339a36cb770a1468e94aba8341be6bc
Ilya Siganov <ilya.blan4@gmail.com>
2021-10-03 10:01:33 +0600

    add coins scores per pcpart and set right values for scores, currency

--- Assets/Scripts/Currency.cs
+++ Assets/Scripts/Currency.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Currency", menuName="Currency", order=52)]
+[CreateAssetMenu(fileName = "New Currency", menuName = "Currency", order = 52)]
 public class Currency : ScriptableObject
 {
     public string currencyName;
     public double USDRate;
+
+    public double cpuScore;
+    public double ramScore;
+    public double gpuScore;
+    public double diskScore;
 }
