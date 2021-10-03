670e1e982d13f7f6f697fdab9a41aaf0d7722a20
Ilya Siganov <ilya.blan4@gmail.com>
2021-10-03 11:48:19 +0600

    add miner to PCManager

--- Assets/Scripts/PCManager.cs
+++ Assets/Scripts/PCManager.cs
@@ -1,12 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PCManager : MonoBehaviour
 {
-    private List<PC> computers;
+    private List<PC> computers = new List<PC>();
+    [SerializeField]
+    private double incomePerSec = 0;
+    public double IncomePerSec => incomePerSec;
 
-    public void AddComputer(PC computer) {
+    [SerializeField]
+    private double balance = 0;
+    public double Balance => balance;
+
+    public void AddComputer(PC computer)
+    {
         computers.Add(computer);
+    }
+
+    // Update is called once per frame
+    // And update current income per sec and balance
+    void UpdateBalance()
+    {
+        incomePerSec = computers.Aggregate(0.0, (acc, pc) => acc + pc.GetDollarsPerSec());
+        balance += incomePerSec * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        UpdateBalance();
     }
 }
