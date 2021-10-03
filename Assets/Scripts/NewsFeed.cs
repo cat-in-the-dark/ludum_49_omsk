b692140975af260e317703a7e8d62a29bbbbc20f
Ilya Siganov <ilya.blan4@gmail.com>
2021-10-03 16:08:01 +0600

    add news script

---
+++ Assets/Scripts/NewsFeed.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewsFeed : MonoBehaviour
+{
+    string text = "";
+    int showTextLength = 1;
+    double timer = 0;
+    const double timePerSymbol = 0.02;
+
+    private Text textField;
+    public GameObject newsScreenHolder;
+
+    public void SetText(string text_)
+    {
+        text = text_;
+        showTextLength = 1;
+        timer = 0;
+        newsScreenHolder.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = newsScreenHolder.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timePerSymbol && showTextLength < text.Length)
+        {
+            timer = 0;
+            textField.text = text.Substring(0, showTextLength);
+            showTextLength += 1;
+        }
+    }
+
+    public void HideNews()
+    {
+        newsScreenHolder.SetActive(false);
+    }
+}
