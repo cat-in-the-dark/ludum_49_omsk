8b8a84ae4cd45731cf13c9771d7711fc9569ca03
Ilya Siganov <ilya.blan4@gmail.com>
2021-10-03 10:26:32 +0600

    add func to calc mining money

--- Assets/Scripts/PC.cs
+++ Assets/Scripts/PC.cs
@@ -13,71 +13,88 @@ public class PC
     private List<GpuPart> gpuSlots = new List<GpuPart>();
     private List<DiskPart> diskSlots = new List<DiskPart>();
 
-    public bool AddCpu(CpuPart cpuPart) {
-        if (cpuSlots.Count < motherBoard.CpuSlots) {
+    public bool AddCpu(CpuPart cpuPart)
+    {
+        if (cpuSlots.Count < motherBoard.CpuSlots)
+        {
             cpuSlots.Add(cpuPart);
             return true;
         }
         return false;
     }
 
-    public bool AddRam(RamPart ramPart) {
-        if (ramSlots.Count < motherBoard.RamSlots) {
+    public bool AddRam(RamPart ramPart)
+    {
+        if (ramSlots.Count < motherBoard.RamSlots)
+        {
             ramSlots.Add(ramPart);
             return true;
         }
         return false;
     }
 
-    public bool AddGpu(GpuPart gpuPart) {
-        if (gpuSlots.Count < motherBoard.GpuSlots) {
+    public bool AddGpu(GpuPart gpuPart)
+    {
+        if (gpuSlots.Count < motherBoard.GpuSlots)
+        {
             gpuSlots.Add(gpuPart);
             return true;
         }
         return false;
     }
 
-    public bool AddDisk(DiskPart diskPart) {
-        if (diskSlots.Count < motherBoard.DiskSlots) {
+    public bool AddDisk(DiskPart diskPart)
+    {
+        if (diskSlots.Count < motherBoard.DiskSlots)
+        {
             diskSlots.Add(diskPart);
             return true;
         }
         return false;
     }
 
-    public void ReplaceCpu(CpuPart cpuPart, int index) {
+    public void ReplaceCpu(CpuPart cpuPart, int index)
+    {
         cpuSlots[index] = cpuPart;
     }
 
-    public void ReplaceRam(RamPart ramPart, int index) {
+    public void ReplaceRam(RamPart ramPart, int index)
+    {
         ramSlots[index] = ramPart;
     }
 
-    public void ReplaceGpu(GpuPart gpuPart, int index) {
+    public void ReplaceGpu(GpuPart gpuPart, int index)
+    {
         gpuSlots[index] = gpuPart;
     }
 
-    public void ReplaceDisk(DiskPart diskPart, int index) {
+    public void ReplaceDisk(DiskPart diskPart, int index)
+    {
         diskSlots[index] = diskPart;
     }
 
-    public List<CpuPart> GetCpuSlots() {
+    public List<CpuPart> GetCpuSlots()
+    {
         return cpuSlots;
     }
 
-    public List<RamPart> GetRamSlots() {
+    public List<RamPart> GetRamSlots()
+    {
         return ramSlots;
     }
 
-    public List<GpuPart> GetGpuSlots() {
+    public List<GpuPart> GetGpuSlots()
+    {
         return gpuSlots;
     }
 
-    public List<DiskPart> GetDiskSlots() {
+    public List<DiskPart> GetDiskSlots()
+    {
         return diskSlots;
     }
 
-    public bool CheckPower() {
+    public bool CheckPower()
+    {
         double psuPower = powerSupply.Energy;
 
         double cpuPower = cpuSlots.Aggregate(0.0, (acc, cpu) => acc + cpu.Energy);
@@ -86,5 +103,28 @@ public class PC
         double diskPower = diskSlots.Aggregate(0.0, (acc, disk) => acc + disk.Energy);
 
         return psuPower >= (cpuPower + ramPower + gpuPower + diskPower);
+    }
+
+    public bool CanMine()
+    {
+        // check if we have enough power and working pc
+        return true;
+    }
+
+    public double GetDollarsPerSecFor(Currency currency)
+    {
+        if (!CanMine()) return 0;
+
+        double cpuPower = cpuSlots.Aggregate(0.0, (acc, obj) => acc + obj.CpuPower);
+        double ramPower = ramSlots.Aggregate(0.0, (acc, obj) => acc + obj.CpuRam);
+        double diskPower = diskSlots.Aggregate(0.0, (acc, obj) => acc + obj.DiskSize);
+        double gpuPower = gpuSlots.Aggregate(0.0, (acc, obj) => acc + obj.CudaPower);
+
+        cpuPower *= currency.cpuScore;
+        ramPower *= currency.ramScore;
+        diskPower *= currency.diskScore;
+        gpuPower *= currency.gpuScore;
+
+        return currency.USDRate * (cpuPower + ramPower + diskPower + gpuPower);
     }
 }
